0a97a692223318dedc0543e0185db30dded473df
Evgeny Gavriluk <evgeny_gavriluk@mail.ru>
2023-10-17 17:36:41 +0300

    it4

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -13,15 +13,15 @@ import (
 func parseFlags() {
     // регистрируем переменную flagRunAddr 
     // как аргумент -a со значением :8080 по умолчанию
-    flag.StringVar(&flagRunAddr, "a", "localhost:8080", "address and port to run server")
-	flag.StringVar(&flagShortAddr, "s", "localhost:8080", "base address and port to short URL")
+    flag.StringVar(&flagRunAddr, "a", "http://localhost:8080", "address and port to run server")
+	flag.StringVar(&flagShortAddr, "s", "http://localhost:8080", "base address and port to short URL")
     // парсим переданные серверу аргументы в зарегистрированные переменные
     flag.Parse()
 
 	fmt.Printf("flagRunAddr = %s", flagRunAddr)
 	fmt.Printf("flagShortAddr = %s", flagShortAddr)
 
-	if string(flagShortAddr)=="localhost:8080" {
+	if string(flagShortAddr)=="http://localhost:8080" {
 		flagShortAddr = flagRunAddr
 		fmt.Printf("flagShortAddr = %s", flagShortAddr)
 	}
